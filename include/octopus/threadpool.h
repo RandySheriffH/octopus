fe657a44a3f82415761afc7593cb790edf8030db
Randy Shuai <rashuai@microsoft.com>
2023-02-03 15:28:59 -0800

    tune num_spin

--- include/octopus/threadpool.h
+++ include/octopus/threadpool.h
@@ -434,7 +434,8 @@ namespace octopus {
 			TaskPool* task_pool = {};
 			ThreadData& thread_data = __thread_datas[index - 1];
 			thread_data.tid = std::this_thread::get_id();
-			const size_t num_spin = 2 * __num_thread;
+			//const size_t num_spin = 2 * __num_thread;
+			const size_t num_spin = 8;
 
 			while (!thread_data.exit) {
 				size_t counter_idel = 0;
